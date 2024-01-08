fc20e0c0083b4dc1ce2e1bd7b25acbe2ca8f5629
Sherman Chen <schen@simviu.com>
2024-01-08 16:50:53 +0800

    add grip

--- mycobot_280/mycobot_280pi_moveit/scripts/sync_plan.py
+++ mycobot_280/mycobot_280pi_moveit/scripts/sync_plan.py
@@ -3,13 +3,36 @@
 import time
 import rospy
 from sensor_msgs.msg import JointState
-
 from pymycobot.mycobot import MyCobot
 
+import math
+
+GRIP_PWM0 = 2050
+GRIP_PWM1 = 1300
+GRIP_DOPEN = 9
+GRIP_DCLOSE = -40
+
+SPD = 80 # 1-100
 
 mc = None
+#------
+def set_grip(mc, d):
+    pwm = GRIP_PWM0 * (1- d)  + d * GRIP_PWM1
+    pwm = int(pwm)
+    print("set grip pwm:", pwm)
+    #mc.set_encoder(7, pwm)
+
+#----
+def calc_grip(r):
+    #gv = int(abs(-0.7 - data_list[6])*117)
+    gd = 180 * r / math.pi
+    print("grip dgr = ", gd)
+    gv = ( gd - GRIP_DOPEN ) / ( GRIP_DCLOSE - GRIP_DOPEN)
+    print("grip val = ", gv)
+    return gv
 
 
+#-------
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "%s", data)
     data_list = []
@@ -18,9 +41,17 @@ def callback(data):
         #     value *= -1
         data_list.append(value)
 
+    print("send_rdians():", data_list)
     mc.send_radians(data_list, 80)
 
+    #----
+    gr = data_list[6]
+    print("grip radian gr=", gr)
+    gv = calc_grip(gr)
+    mc.set_gripper_value(int(gv*100), SPD)
 
+
+#-------
 def listener():
     global mc
     rospy.init_node("mycobot_reciver", anonymous=True)
